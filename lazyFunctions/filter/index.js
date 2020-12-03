b0c59e5e87a5745895becd77a4674436c2e95ab3
wkddngus5 <ravi@inflearn.com>
2020-12-03 17:19:08 +0900

    feat: lazy filter

---
+++ lazyFunctions/filter/index.js
@@ -0,0 +1,9 @@
+function* filter(collection, predicate) {
+  for (const value of collection) {
+    if (predicate(value)) {
+      yield value;
+    }
+  }
+}
+
+module.exports = filter;
--- lazyFunctions/lazyFunctions.test.js
+++ lazyFunctions/lazyFunctions.test.js
@@ -1,4 +1,5 @@
 const map = require('./map');
+const filter = require('./filter');
 
 const usersMock = [
   { id: 1, name: 'user1', age: 11 },
@@ -10,9 +11,25 @@ const usersMock = [
 ];
 
 test('map', () => {
+  let called = 0;
   const squared = map([1, 2, 3, 4, 5], (value) => {
-    console.log(value);
+    called++;
     return value * value;
   });
   expect(squared.next().value).toBe(1);
+  expect(squared.next().value).toBe(4);
+  expect(called).toBe(2);
 });
+
+test('filter', () => {
+  let called = 0;
+  const squared = filter([1, 2, 3, 4, 5], (value) => {
+    called++;
+    return value % 2;
+  });
+  expect(squared.next().value).toBe(1);
+  expect(squared.next().value).toBe(3);
+  expect(called).toBe(3);
+});
+
+
